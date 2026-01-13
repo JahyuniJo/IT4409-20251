1b1dc94c2952b5a1c66354fa743050bc13fb11c1
Dieu Hoang <dieu300504@gmail.com>
2026-01-14 04:12:04 +0700

    Change name

--- my-app/backend/src/utils/sendEmail.js
+++ my-app/backend/src/utils/sendEmail.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const sendEmail = async ({ to, otp }) => {
   const html = `
     <div style="font-family:Arial;padding:20px">
-      <h3>DH Story – Đặt lại mật khẩu</h3>
+      <h3>myappIT4409 – Đặt lại mật khẩu</h3>
       <p>Mã OTP của bạn:</p>
       <h1 style="letter-spacing:6px">${otp}</h1>
       <p>Mã có hiệu lực trong 5 phút.</p>
